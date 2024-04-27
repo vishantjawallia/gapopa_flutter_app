643069870a01084e1f3a9340d75b60fb28f902c7
Vishant Jawallia <68182707+vishantjawallia@users.noreply.github.com>
2024-04-28 00:02:47 +0530

    web production

--- lib/routes.dart
+++
@@ -1,8 +0,0 @@
-import 'package:gapopa_flutter_app/views/home/home_view.dart';
-import 'package:gapopa_flutter_app/views/image_detail/image_detail_view.dart';
-import 'package:get/get.dart';
-
-final routes = [
-  GetPage(name: HomeView.routeName, page: () => const HomeView()),
-  GetPage(name: ImageDetailView.routeName, page: () => const ImageDetailView()),
-];
